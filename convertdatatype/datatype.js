c14075d218c47701c22f5747b4126eda71a3461f
Abdullah <kagmi.tiga678@gmail.com>
2026-07-21 16:45:36 +0500

    add: create datatype.js with boolean conversion and logging

---
+++ convertdatatype/datatype.js
@@ -0,0 +1,10 @@
+//let someNumber = 33
+//someNumber = String(someNumber)
+//console.log(someNumber)
+//console.log(typeof someNumber) 
+
+
+let isloggedIn = 1
+let booleanisloggedIn = Boolean(isloggedIn)
+console.log(booleanisloggedIn)
+console.log(typeof booleanisloggedIn)
