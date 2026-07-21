7d89424eeb082f6d7e73aed56ad2b9e5496960af
Abdullah <kagmi.tiga678@gmail.com>
2026-07-21 16:03:02 +0500

    add: initialize second.js with account variables and logging

---
+++ variables/second.js
@@ -0,0 +1,8 @@
+let accountName= "abdullah";
+const accountID= "12345";
+var accountBalance= 1000;
+accountBalance += 500;
+
+console.log("Account Name: " + accountName);
+console.log("Account ID: " + accountID);
+console.log("Account Balance: " + accountBalance);
